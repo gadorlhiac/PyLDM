e77d92399492c21eddee5b8050b8f01582400193
gadorlhiac <gadorlhiac@gmail.com>
2016-10-17 05:16:07 -0700

    Uploaded license

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from distutils.core import setup
 setup(name='PyLDA',
       version='0.1',
       description='Python Based Lifetime Density Analysis of Time-Resolved Ultrafast Data',
-      author='Gabriel Dorlhiac',
-      author_email='gadorlhiac@gmail.com',
+      author='Gabriel Dorlhiac, Clyde Fare',
       url='https://www.github.com/gadorlhiac/pylda',
       packages=['pylda', 'pylda.Fit'],
 
